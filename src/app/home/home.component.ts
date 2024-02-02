3c5e9c07bbab834e5be34eca71f2c8ba623d34f2
Dawood Abdullah <dawood.abdullah@Polestarllp.local>
2024-02-02 22:28:15 +0530

    new chnages maps@

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,14 +18,18 @@ export class HomeComponent implements OnInit {
 
   getProductArr(){
     this.productDetailsArr = [
-      { imgPath: '../../assets/images/image.jpg', value: 1, off:'50%', price:  'Rs 700'},
-      { imgPath: '../../assets/images/image2.jpg', value: 1, off:'50%', price: 'Rs 500'},
-      { imgPath: '../../assets/images/image3.jpg', value: 1, off:'50%', price: 'Rs 100'},
-      { imgPath: '../../assets/images/image4.jpg', value: 1, off:'50%', price: 'Rs 400'},
-      { imgPath: '../../assets/images/image5.jpg', value: 1, off:'50%', price: 'Rs 600'},
-      { imgPath: '../../assets/images/image6.jpg', value: 1, off:'50%', price: 'Rs 700'},
-      { imgPath: '../../assets/images/image2.jpg', value: 1, off:'50%', price: 'Rs 900'},
-      { imgPath: '../../assets/images/image.jpg', value: 1, off:'50%', price: 'Rs 1000'},
+      { imgPath: '../../assets/images/image12.png', value: 1, off:'50%', price:  'Rs 700'},
+      { imgPath: '../../assets/images/image13.jpeg', value: 1, off:'50%', price: 'Rs 500'},
+      { imgPath: '../../assets/images/image14.jpeg', value: 1, off:'50%', price: 'Rs 100'},
+      { imgPath: '../../assets/images/image10.png', value: 1, off:'50%', price: 'Rs 400'},
+      { imgPath: '../../assets/images/image11.png', value: 1, off:'50%', price: 'Rs 600'},
+      { imgPath: '../../assets/images/image17.jpeg', value: 1, off:'50%', price: 'Rs 600'},
+      { imgPath: '../../assets/images/image8.png', value: 1, off:'50%', price: 'Rs 600'},
+      { imgPath: '../../assets/images/image9.png', value: 1, off:'50%', price: 'Rs 700'},
+      { imgPath: '../../assets/images/image15.jpeg', value: 1, off:'50%', price: 'Rs 700'},
+      { imgPath: '../../assets/images/image16.jpeg', value: 1, off:'50%', price: 'Rs 700'},
+      { imgPath: '../../assets/images/image8.png', value: 1, off:'50%', price: 'Rs 900'},
+      { imgPath: '../../assets/images/image10.png', value: 1, off:'50%', price: 'Rs 1000'},
     ]
   }
   
